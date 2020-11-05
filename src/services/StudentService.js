1a0c660449c98d82bcd25a31a9b1bbf14b3730cc
Shaun Lum <shaun.lum@gov.bc.ca>
2020-11-05 08:32:51 -0800

    dist

--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -5,7 +5,8 @@ const apiClient = axios.create({
     withCredentials: false,
     headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Origin": "*"
     }
 })
 
