3a90d3052fad067fe2e270bb3aa7bb24695c8b54
Shaun <shaun.lum@gov.bc.ca>
2020-11-10 11:25:34 -0800

    fix student profile

--- src/views/StudentProfile.vue
+++ src/views/StudentProfile.vue
@@ -11,9 +11,9 @@
       <div>
         <b-card no-body>
           <b-tabs card>
-            <!-- <b-tab title="Profile" active>
+            <b-tab title="Profile" active>
               <b-card-text><StudentInfo/></b-card-text>
-            </b-tab> -->
+            </b-tab>
             <b-tab title="Courses">
               <b-card-text><StudentCourses/></b-card-text>
             </b-tab>
@@ -29,16 +29,14 @@
 // import CourseService from "@/services/CourseService.js";
 import SiteMessage from "@/components/SiteMessage";
 import StudentCourses from "@/components/StudentCourses";
-//import StudentInfo from "@/components/StudentInfo";
+import StudentInfo from "@/components/StudentInfo";
 import { mapGetters } from "vuex";
-//import StudentInfo from "@/components/StudentInfo.js";
 //import { store } from "@/store.js";
 export default {
   components: {
     SiteMessage: SiteMessage,
     StudentCourses: StudentCourses,
-    // StudentInfo: StudentInfo,
-    
+    StudentInfo: StudentInfo,
   },
   data() {
     return {
