779a54430b49241f044a8a4f41c6d4646d82b271
Shaun <shaun.lum@gov.bc.ca>
2020-12-04 13:14:07 -0800

    shaun

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,24 @@
 <template>
-
   <div>
       {{ token }}
   </div>
 </template>
+
 <script>
 // @ is an alias to /src
-import { mapGetters } from "vuex";
 import LoginService from "@/services/LoginService.js";
 export default {
   name: "Login",
   data() {
     return {
-        token: "",
-      }
-    };
+      token: "",
+    }
   },
   created() {
       this.token = LoginService.login();
   },
-  components: {},
-  },
-  methods: {
-  },
-};
+}
 </script>
+
 <style scoped>
 </style>
