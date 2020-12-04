b8784cdabad877e8e0435c2b52d7065240a793d2
Michael Tang <michael.tang@gov.bc.ca>
2020-12-04 15:37:25 -0800

    added token

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -11,9 +11,7 @@ const apiClient = axios.create({
 })
 
 export default {
-    // getCourses() {
-    //     return apiClient.get('/api/v1/courses')
-    // },
+
     login() {
         let requestBody={
               "client_id":process.env.VUE_APP_CLIENT_ID,
@@ -24,7 +22,6 @@ export default {
               };
 
         let url = process.env.VUE_APP_AUTH_HOST;
-        // try {
         apiClient.post(url, qs.stringify(requestBody))
             .then(res => {
                 console.log(res.data.access_token);
@@ -32,29 +29,5 @@ export default {
                 //router.push("/")
             })
             .catch(error => console.log(error))
-        }
-        // catch (error){
-        //    console.error(error);
-        //    console.log("Token request failed!");
-        //    return false ;
-        // }
-    //  }
-    // login () {
-    //     apiClient.post(process.env.KEY_CLOAK_AUTH_HOST, 
-    //     {
-    //       client_id:process.env.KEY_CLOAK_CLIENT_ID,
-    //       client_secret:process.env.KEY_CLOAK_CLIENT_SECRET,
-    //       grant_type:process.env.KEY_CLOAK_GRANT_TYPE,
-    //       username: process.env.KEY_CLOAK_USER,
-    //       password: process.env.KEY_CLOAK_PW,
-    //       returnSecureToken: true
-    //     })
-    //     .then(res => {
-    //       console.log(res)
-    //       return res;
-    //       //router.push("/")
-          
-    //     })
-    //     .catch(error => console.log(error))
-    // }
+    }
 }
