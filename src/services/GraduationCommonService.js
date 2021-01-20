baab922a3b3819758c0b17a9cc88a9264d9c1fd1
Shaun <shaun.lum@gov.bc.ca>
2021-01-20 10:34:44 -0800

    added commons service

---
+++ src/services/GraduationCommonService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: process.env.VUE_APP_GRADUATION_COMMON_API_HOST,
+})
+export default {
+  getAchievementReport(pen, token) {
+    const headers = { Accept: 'application/json','Content-Type': 'application/json',"Access-Control-Allow-Origin": "*",'Authorization': 'Bearer '+ token, "responseType": "arraybuffer" }
+    return apiClient.get('/api/v1/common/studentreport/?pen=' + pen + '&reportType=studentachievement',{ headers, responseType: 'arraybuffer'});
+  },
+}
--- src/services/StudentService.js
+++ src/services/StudentService.js
@@ -11,6 +11,7 @@ export default {
         //console.log("TOKEN IN SERVICE getStudentByPen " + token);
         return apiClient.get('/api/v1/' + pen,{ headers });
     },
+
     getStudentsByAdvancedSearch(advancedSearchInput, token){
         const headers = { Accept: 'application/json','Content-Type': 'application/json','Access-Control-Allow-Origin': '*','Authorization': 'Bearer '+ token }
         //console.log("TOKEN IN SERVICE getStudentsByAdvancedSearch " + token);
