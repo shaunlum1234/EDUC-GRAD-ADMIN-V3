c3c2e50d89531c8026f0a5c91983aa0bfadf2d60
Shaun <shaun.lum@gov.bc.ca>
2021-03-03 16:20:20 -0800

    added OCP4 code

--- dist/server.js
+++ dist/server.js
@@ -142,5 +142,5 @@ app.use('/api/course', createProxyMiddleware({
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.listen(port, ipaddress, function() {
-  console.log(`Server is running on port ` + port);
+  console.log("Server is running on " + ipaddress + " port " + port);
 });
