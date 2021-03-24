629123d967939a46105641d1ec56e0d3f76db0de
Michael Tang <michael.tang@gov.bc.ca>
2021-03-24 08:53:36 -0700

    console log

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -106,7 +106,7 @@
           })
           // eslint-disable-next-line no-unused-vars
           .catch((error) => {
-            //console.log('There was an error:' + error.response);
+            console.log('There was an error:' + error.response.status);
           });
       },
       getAllCourseRestrictions(){
