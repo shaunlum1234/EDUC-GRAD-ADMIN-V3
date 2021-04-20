5d1f2b81c6d216e32d14e5b8b0e62ed6f122dba3
Shaun <shaun.lum@gov.bc.ca>
2021-04-20 15:52:50 -0700

    added missing files

---
+++ src/components/GraduationProgramCourses.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+  <h1>Rule {{selectedRule}}</h1>
+  <div v-for="item in graduationProgramRuleCourses" :key="item.requirementName">
+    {{item.ruleCode}}
+    {{item.requirementName}}
+    {{item.programCode}}
+    {{item.specialProgramCode}}
+  </div>
+
+</div>
+</template>
+
+<script>
+//import GradeService from "@/services/GradeService.js";
+import ProgramManagementService from "@/services/ProgramManagementService.js";
+import {
+    mapGetters
+} from "vuex";
+export default {
+  name: "GraduationProgramCourses",
+  props: {    
+  },
+  components: {
+
+  },
+
+
+  
+  computed: {...mapGetters({
+    token: "getToken", 
+  })},
+  data: function () {
+    return {
+      opened: [],
+      selectedRule:"",
+      graduationProgramRuleCourses:[],
+    };
+  },
+  created() {
+    console.log("RULE" + this.$route.params.rule)
+    this.selectedRule = this.$route.params.rule
+    ProgramManagementService.getGraduationProgramRuleCourses(this.$route.params.rule, this.token)
+      .then((response) => {
+        console.log(response);
+        this.graduationProgramRuleCourses = response.data;
+    });
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style>
+.table th {
+  border-bottom: 2px solid #38598a !important;
+}
+</style>
