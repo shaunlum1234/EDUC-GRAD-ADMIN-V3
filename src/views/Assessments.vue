ae5a80215a8da7d7acaa07da5abf3213a9d685a5
Michael Tang <michael.tang@gov.bc.ca>
2021-04-22 16:57:32 -0700

    fixed the bdate and assessments

--- src/views/Assessments.vue
+++ src/views/Assessments.vue
@@ -7,21 +7,40 @@
           <b-tab title="Assessment search" active>
             <b-card-text>
               <div class="row col-12">
-                <label class="float-left w-100">Search by assessments code:</label>
-                <b-input class="col-10 my-2" v-model="assmtCode" placeholder="" />
-                <b-button variant="primary" class="m-2" @click="getAllAssessment">Search</b-button>
+                <label class="float-left w-100"
+                  >Search by assessments code:
+                </label>
+                <b-input
+                  class="col-10 my-2"
+                  v-model="assmtCode"
+                  placeholder=""
+                />
+                <b-button
+                  variant="primary"
+                  class="m-2"
+                  @click="getAllAssessment"
+                  >Search
+                </b-button>
               </div>
-              <DisplayTable title="Courses" v-bind:items="courses" v-bind:fields="courseFields" id="courseCode"
-                v-bind:role="role"></DisplayTable>
+              <DisplayTable
+                title="Assessments"
+                v-bind:items="assessments"
+                v-bind:fields="assessmentFields"
+                id="assessmentCode"
+                v-bind:role="role"
+              >
+              </DisplayTable>
             </b-card-text>
           </b-tab>
-          <b-tab title="Course restrictons">
+          <b-tab title="Assessment requirements">
             <b-card-text>
-              <DisplayTable title="Course restriction" v-bind:items="courseRestrictions"
-                v-bind:fields="courseRestrictionFields" id="courseRestrictionId"
-                create="createCourseRestriction" delete="deleteCourseRestriction" update="updateCourseRestriction">
+              <DisplayTable
+                title="Assessment requirements"
+                v-bind:items="assessmentRequirements"
+                v-bind:fields="assessmentRequirementsFields"
+                id="assessmentCode"
+              >
               </DisplayTable>
-
             </b-card-text>
           </b-tab>
         </b-tabs>
@@ -31,61 +50,138 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 
-import {
-    mapGetters
-} from "vuex";
+import AssessmentService from "@/services/AssessmentService.js";
+import DisplayTable from "@/components/DisplayTable.vue";
+export default {
+  name: "assessments",
+  components: {
+    DisplayTable: DisplayTable,
+  },
+  data() {
+    return {
+      assessments: {},
+      assessmentRequirements: {},
+      assmtCode: "",
+      assessmentFields: [
+        {
+          key: "assessmentCode",
+          label: "Assessment code",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "assessmentName",
+          label: "Assessment name",
+          sortable: true,
+          class: "text-center",
+        },
+        {
+          key: "language",
+          label: "Language",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "startDate",
+          label: "Start date",
+          sortable: true,
+          class: "text-center",
+        },
+        {
+          key: "endDate",
+          label: "End date",
+          sortable: true,
+          class: "text-center",
+        },
+      ],
 
-import AssessmentService from '@/services/AssessmentService.js';
-
-  export default {
-    name: "assessments",
-    components: {
-      
+      assessmentRequirementsFields: [
+        {
+          key: "assessmentCode",
+          label: "Assessment code",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "assessmentName",
+          label: "Assessment name",
+          sortable: true,
+          class: "text-center",
+        },
+        {
+          key: "ruleCode",
+          label: "Rule#",
+          sortable: true,
+        },
+        {
+          key: "requirementName",
+          label: "Requirement name",
+          sortable: true,
+          class: "text-center",
+        },
+        {
+          key: "requirementProgram",
+          label: "Requirement program",
+          sortable: true,
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters({
+      token: "getToken",
+      role: "getRoles"
+    }),
+  },
+  created() {
+    this.getAllAssessment();
+    this.getAllAssessmentReqs();
+  },
+  methods: {
+    getAllAssessment() {
+      AssessmentService.getAllAssesments(this.token)
+        .then((response) => {
+          this.assessments = response.data;
+        })
+        // eslint-disable-next-line
+        .catch((error) => {
+          this.$bvToast.toast("ERROR " + error.response.statusText, {
+            title: "ERROR" + error.response.status,
+            variant: "danger",
+            noAutoHide: true,
+          });
+        });
     },
-    data() {
-      return {
-          assessments:{},
-          assmtCode:"",
-      }
+    getAllAssessmentReqs() {
+      AssessmentService.getAllAssesmentRequirements(this.token)
+        .then((response) => {
+          this.assessmentRequirements = response.data;
+        })
+        // eslint-disable-next-line
+        .catch((error) => {
+          this.$bvToast.toast("ERROR " + error.response.statusText, {
+            title: "ERROR" + error.response.status,
+            variant: "danger",
+            noAutoHide: true,
+          });
+        });
     },
-    computed: {
-        ...mapGetters({
-            token: "getToken"
-        }),
-    },
-    created() {
-      this.getAllAssessment();
-    },
-    methods: {
-        getAllAssessment(){
-            AssessmentService.getAllAssesments(this.token)
-            .then((response) => {
-                this.assessments = response.data;
-            })
-            // eslint-disable-next-line
-            .catch((error) => {
-                this.$bvToast.toast("ERROR " + error.response.statusText, {
-                title: "ERROR" + error.response.status,
-                variant: 'danger',
-                noAutoHide: true,
-                });
-            });
-        }
-    },
-  };
+  },
+};
 </script>
 
 <style scoped>
-  .close-record {
-    float: right;
-  }
+.close-record {
+  float: right;
+}
 
-  .tab-loading {
-    color: green !important;
-  }
+.tab-loading {
+  color: green !important;
+}
 
-  .profile-name {
-    padding-bottom: 10px;
-  }
+.profile-name {
+  padding-bottom: 10px;
+}
 </style>
