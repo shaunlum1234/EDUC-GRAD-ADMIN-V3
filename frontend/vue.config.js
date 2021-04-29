eb993699332680cfa9ce192554ca93e199d8bd5b
Shaun <shaun.lum@gov.bc.ca>
2021-04-29 09:34:40 -0700

    changes to caddyfile and vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -16,21 +16,46 @@ module.exports = {
         target: process.env.VUE_APP_STUDENTS_API_HOST,
         changeOrigin: true,
       },
-      '/api/student-assessments': {
+      '/api/studentassessment': {
         target: process.env.VUE_APP_STUDENT_ASSESSMENTS_API_HOST,
         changeOrigin: true,
-        pathRewrite: {
-          '^/api/student-assessments': ''
-        }
       },
-      '/api/studentexam': {
-          target: process.env.VUE_APP_STUDENT_EXAMS_API_HOST,
-          changeOrigin: true,
-          pathRewrite: {
-              '^/api/studentexam': ''
-            }
-        },
-
+      '/api/v1/school': {
+        target: process.env.VUE_APP_SCHOOL_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/psi': {
+        target: process.env.VUE_APP_PSI_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/programmanagement': {
+        target: process.env.VUE_APP_PROGRAM_MANAGEMENT_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/gradstatus': {
+        target: process.env.VUE_APP_GRADUATION_STATUS_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/common': {
+        target: process.env.VUE_APP_GRADUATION_COMMON_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/course': {
+        target: process.env.VUE_APP_COURSE_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/studentcourse': {
+        target: process.env.VUE_APP_COURSE_ACHIEVEMENT_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/assessment': {
+        target: process.env.VUE_APP_ASSESSMENT_API_HOST,
+        changeOrigin: true,
+      },
+      '/api/v1/graduate/': {
+        target: process.env.VUE_APP_GRADUATION_API_HOST,
+        changeOrigin: true,
+      },
     }
   },
   transpileDependencies: ['vuetify'],
