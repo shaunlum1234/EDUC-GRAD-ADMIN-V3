bdb640b9ebc9f2990c3fe2f6d4eb69363b98022e
Shaun <shaun.lum@gov.bc.ca>
2021-05-27 15:28:37 -0700

    school

--- frontend/src/services/SchoolService.js
+++ frontend/src/services/SchoolService.js
@@ -23,21 +23,21 @@ export default {
         
         const headers = { Accept: 'application/json','Content-Type': 'application/json',"Access-Control-Allow-Origin": "*",'Authorization': 'Bearer '+ token }
         let queryString = ""
-        for (var key in searchInput ) {
-  
-             
-                if(searchInput[key]){
-                //     let contains = (searchInput[key].contains) ? "*":"";
-                     queryString 
-                         += key 
-                         + "=" 
-                         + searchInput[key]
-                //         + contains
-                         + "&";
-                 }
-            
+        for (var key in searchInput) {
+            if (searchInput.hasOwnProperty(key)) {
+                if(searchInput[key].value){
+                    let contains = (searchInput[key].contains) ? "*":"";
+                    queryString 
+                        += key 
+                        + "=" 
+                        + searchInput[key].value
+                        + contains
+                        + "&";
+                }
+            }
         }
         queryString = queryString.slice(0, -1); //remove trailing ampersand
+        console.log('/api/v1/school/search?' + queryString);
     //    console.log('/api/v1/school/?' + queryString);
         return apiClient.get('/api/v1/school/search?' + queryString,{ headers });
     }
