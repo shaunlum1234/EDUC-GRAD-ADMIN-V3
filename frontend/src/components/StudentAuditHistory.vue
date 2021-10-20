57a78e92d8ca4e28eb11487291d536e4f105852e
Michael Tang <michael.tang@gov.bc.ca>
2021-10-20 13:30:03 -0700

    cleaned up codebase

--- frontend/src/components/StudentAuditHistory.vue
+++ frontend/src/components/StudentAuditHistory.vue
@@ -1,45 +1,35 @@
 <template>
   <div class="container">
     <div class="col-12" v-for="(value, index) in changeHistory.slice().reverse()" :key="value.historyID">
-          <div class="row col-12 py-2" :header="studentHistory[index+1].historyID">
-   
-      
-              <!-- <li>{{v.user}}</li> -->
-              <div class="col-4 border-bottom">
-                Edited by <strong>{{studentHistory[index+1].activityCode}}</strong> on<br/>
-                {{studentHistory[index+1].createDate || formatTime}}
-              </div>
-              <div class="float-left col-8 border-bottom">
-                      <div class="float-right w-25">
-                        <b-button v-b-toggle="'collapse-'+ studentHistory[index+1].historyID" variant="primary">View</b-button>
-                      </div>
-                <div v-for="v in value" :key="v.historyID" class="">
-                 
-                  <div class="" v-if="v.pathTo != 'updateDate' 
-                      && v.pathTo != 'createDate' 
-                      && v.pathTo != 'historyID'
-                      && v.pathTo != 'studentGradData'
-                      && v.pathTo != 'activityCode'
-                      && v.pathTo != 'recalculateGradStatus'
-                      ">
-                      <div class="w-25 float-left"> <strong>{{v.pathTo | formatSetenceCase}}</strong>:</div>
-                      <div class="w-50 float-left"> {{v.lhs==null?"_____":v.lhs}} <i class="fas fa-arrow-right"></i> {{v.rhs == null?"_____":v.rhs}}</div>
-                
-                      <div class="w-100 float-left">
-                        <b-collapse :id="'collapse-' + studentHistory[index+1].historyID" class="mt-2">
-                          <pre>{{JSON.stringify(studentHistory[index+1], null, '\t')}}</pre>
-                        </b-collapse>
-                      </div>
-                  </div>  
-           
+      <div class="row col-12 py-2" :header="studentHistory[index+1].historyID">
+        <div class="col-4 border-bottom">
+          Edited by <strong>{{studentHistory[index+1].activityCode}}</strong> on<br/>
+          {{studentHistory[index+1].createDate || formatTime}}
+        </div>
+        <div class="float-left col-8 border-bottom">
+                <div class="float-right w-25">
+                  <b-button v-b-toggle="'collapse-'+ studentHistory[index+1].historyID" variant="primary">View</b-button>
                 </div>
-                  
-                
-                
+          <div v-for="v in value" :key="v.historyID" class="">
+            <div class="" v-if="v.pathTo != 'updateDate' 
+              && v.pathTo != 'createDate' 
+              && v.pathTo != 'historyID'
+              && v.pathTo != 'studentGradData'
+              && v.pathTo != 'activityCode'
+              && v.pathTo != 'recalculateGradStatus'
+              ">
+              <div class="w-25 float-left"> <strong>{{v.pathTo | formatSetenceCase}}</strong>:</div>
+              <div class="w-50 float-left"> {{v.lhs==null?"blank":v.lhs}} <i class="fas fa-arrow-right"></i> {{v.rhs == null?"blank":v.rhs}}</div>
+        
+              <div class="w-100 float-left">
+                <b-collapse :id="'collapse-' + studentHistory[index+1].historyID" class="mt-2">
+                  <pre>{{JSON.stringify(studentHistory[index+1], null, '\t')}}</pre>
+                </b-collapse>
               </div>
-               
-  
-          </div>
+            </div>  
+          </div> 
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -47,6 +37,7 @@
 <script>
 import { mapGetters } from "vuex";
 import { DeepDiff } from 'deep-diff';
+import sharedMethods from '../sharedMethods';
 import StudentAuditHistoryService from "@/services/StudentAuditHistoryService.js";
 
 export default {
@@ -66,6 +57,9 @@ export default {
         studentHistory: "",
         studentHistoryChangeCount:"",
         OptionalProgramHistory:"",
+        isEdit:false,
+        isDelete:false,
+        isAdd:false,
         changeHistory:[],
         testHistory:[],
          fields: [
@@ -86,97 +80,44 @@ export default {
     };
   },
   created() {
-      const studentIdFromURL = this.$route.params.studentId;
-      this.loadStudentHistory(studentIdFromURL);
-    //   this.loadStudentOptionalProgramHistory(studentIdFromURL);
+    const studentIdFromURL = this.$route.params.studentId;
+    this.showNotification = sharedMethods.showNotification
+    this.loadStudentHistory(studentIdFromURL);
+    this.loadStudentOptionalProgramHistory(studentIdFromURL);
   },
   methods: {
     loadStudentHistory(studentIdFromURL){
         StudentAuditHistoryService.getStudentHistory(studentIdFromURL, this.token).then(
             (response) => {
-                this.studentHistory = response.data;
-                this.studentHistoryChangeCount = this.studentHistory.length
-                for (let i = 0; i < this.studentHistoryChangeCount - 1; i++) {
-                    // this.changeHistory.splice(i,1,this.diff(this.studentHistory[i], this.studentHistory[i + 1]))  
-                    // this.changeHistory = DeepDiff(this.studentHistory[i], this.studentHistory[i + 1]); 
-                    var x = DeepDiff(this.studentHistory[i], this.studentHistory[i + 1]);
-                    // x.push({"user" : this.studentHistory[i].createUser});
-                    // x.push({"createDate" : this.studentHistory[i].createDate});
-                    // x.push({"path" : x[i].path[0]});
-                    // console.log("Test")
-                    // console.log(x[i].path[0]);
-                    this.changeHistory.splice(i,1,x)     
-                }
-                for (let j = 0; j < this.changeHistory.length ; j++) {  
-                    for (let k = 0; k < this.changeHistory[j].length; k++) { 
-                        console.log('j') 
-                        console.log(j) 
-                        console.log('k') 
-                        console.log(k)  
-                        this.changeHistory[j][k].pathTo = this.changeHistory[j][k].path[0]
-                    }
-                    
-                }
-                // console.log(this.changeHistory)
-
-            }
-            
+              this.studentHistory = response.data;
+              this.studentHistoryChangeCount = this.studentHistory.length
+              for (let i = 0; i < this.studentHistoryChangeCount - 1; i++) {
+                  var x = DeepDiff(this.studentHistory[i], this.studentHistory[i + 1]);
+                  this.changeHistory.splice(i,1,x)     
+              }
+              for (let j = 0; j < this.changeHistory.length ; j++) {  
+                  for (let k = 0; k < this.changeHistory[j].length; k++) { 
+                      console.log('j') 
+                      console.log(j) 
+                      console.log('k') 
+                      console.log(k)  
+                      this.changeHistory[j][k].pathTo = this.changeHistory[j][k].path[0]
+                  }                 
+              }
+            }    
         ).catch((error) => {
-                console.error(error);  
+          this.showNotification("danger", "There was an error with the web service: " + error);
         });
     },
     loadStudentOptionalProgramHistory(studentIdFromURL){
-        StudentAuditHistoryService.getStudentOptionalProgramHistory(studentIdFromURL, this.token).then(
-            (response) => {
-                this.OptionalProgramHistory = response.data;              
-            }
-        ).catch((error) => {
-            
-            if(error.response.status){
-            // this.$bvToast.toast("ERROR " + error.response.statusText, {
-            //     title: "ERROR" + error.response.status,
-            //     variant: 'danger',
-            //     noAutoHide: true,
-            // });
-                // console.error(error.response);
-            }
-        });
-    },
-    // getObjectDiff(obj1, obj2) {
-    // const diff = Object.keys(obj1).reduce((result, key) => {
-    //     if (!obj2.hasOwnProperty(key)) {
-    //         result.push(key);
-    //     } else if (_.isEqual(obj1[key], obj2[key])) {
-    //         const resultKeyIndex = result.indexOf(key);
-    //         result.splice(resultKeyIndex, 1);
-    //     }
-    //     return result;
-    // }, Object.keys(obj2));
-
-    // return diff;
-    // }, 
-    diff(obj1, obj2) {
-    const result = {}; 
-    if (Object.is(obj1, obj2)) {
-        return undefined;
-    }
-    if (!obj2 || typeof obj2 !== 'object') {
-        return obj2;
-    }
-    Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
-        if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
-            result[key] = obj2[key];
+      StudentAuditHistoryService.getStudentOptionalProgramHistory(studentIdFromURL, this.token).then(
+        (response) => {
+            this.OptionalProgramHistory = response.data;              
         }
-        if(typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
-            const value = this.diff(obj1[key], obj2[key]);
-            if (value !== undefined) {
-                result[key] = value;
-            }
-        }
-    });
-    return result;
-}
-        
+      ).catch((error) => {
+        this.showNotification("danger", "There was an error with the web service: " + error);
+      });
+    },   
   },
 };
 </script>
@@ -194,4 +135,5 @@ export default {
 .highlight {
   background: aliceblue !important;
 }
+
 </style>
