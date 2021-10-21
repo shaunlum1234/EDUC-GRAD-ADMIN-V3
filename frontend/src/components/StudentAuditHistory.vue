75603c424b3481ebbbe378ca0546c9723770e435
Michael Tang <michael.tang@gov.bc.ca>
2021-10-21 11:09:11 -0700

    plugged in optional history

--- frontend/src/components/StudentAuditHistory.vue
+++ frontend/src/components/StudentAuditHistory.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
   {{auditOptionalProgramsHistory}}
+  <h3>Student change history</h3>
     <div class="col-12" v-for="(value, index) in changeHistory.slice().reverse()" :key="value.historyID">
       <div class="row col-12 py-2" :header="studentHistory[index+1].historyID">
         <div class="col-4 border-bottom">
@@ -25,6 +26,39 @@
               <div class="w-100 float-left">
                 <b-collapse :id="'collapse-' + studentHistory[index+1].historyID" class="mt-2">
                   <pre>{{JSON.stringify(studentHistory[index+1], null, '\t')}}</pre>
+                </b-collapse>
+              </div>
+            </div>  
+          </div> 
+        </div>
+      </div>
+    </div>
+    <h3>Optional program change history</h3>
+    <!-- Optional Program history -->
+    <div class="col-12" v-for="(value, index) in optionalProgramChangeHistory.slice().reverse()" :key="value.historyID">
+      <div class="row col-12 py-2" :header="optionalProgramHistory[index+1].historyID">
+        <div class="col-4 border-bottom">
+          Edited by <strong>{{optionalProgramHistory[index+1].activityCode}}</strong> on<br/>
+          {{optionalProgramHistory[index+1].createDate || formatTime}}
+        </div>
+        <div class="float-left col-8 border-bottom">
+                <div class="float-right w-25">
+                  <b-button v-b-toggle="'collapse-'+ optionalProgramHistory[index+1].historyID" variant="primary">View</b-button>
+                </div>
+          <div v-for="v in value" :key="v.historyID" class="">
+            <div class="" v-if="v.pathTo != 'updateDate' 
+              && v.pathTo != 'createDate' 
+              && v.pathTo != 'historyID'
+              && v.pathTo != 'studentGradData'
+              && v.pathTo != 'activityCode'
+              && v.pathTo != 'recalculateGradStatus'
+              ">
+              <div class="w-25 float-left"> <strong>{{v.pathTo | formatSetenceCase}}</strong>:</div>
+              <div class="w-50 float-left"> {{v.lhs==null?"blank":v.lhs}} <i class="fas fa-arrow-right"></i> {{v.rhs == null?"blank":v.rhs}}</div>
+        
+              <div class="w-100 float-left">
+                <b-collapse :id="'collapse-' + optionalProgramHistory[index+1].historyID" class="mt-2">
+                  <pre>{{JSON.stringify(optionalProgramHistory[index+1], null, '\t')}}</pre>
                 </b-collapse>
               </div>
             </div>  
@@ -112,7 +146,13 @@ export default {
                   var x = DeepDiff(this.optionalProgramHistory[i], this.optionalProgramHistory[i + 1]);
                   this.optionalProgramChangeHistory.splice(i,1,x)     
             } 
-            console.log(this.optionalProgramChangeHistory)   
+            
+            for (let j = 0; j < this.optionalProgramChangeHistory.length ; j++) {  
+              for (let k = 0; k < this.optionalProgramChangeHistory[j].length; k++) { 
+                  this.optionalProgramChangeHistory[j][k].pathTo = this.optionalProgramChangeHistory[j][k].path[0]
+              }                 
+            }
+            console.log(this.optionalProgramChangeHistory) 
                
         }
       ).catch((error) => {
