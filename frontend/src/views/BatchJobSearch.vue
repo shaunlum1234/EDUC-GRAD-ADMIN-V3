adb1d1d51c14da49227f4660a29f8e80193e8835
Michael Tang <michael.tang@gov.bc.ca>
2022-02-04 11:11:34 -0800

    new component for Batch jobs

---
+++ frontend/src/views/BatchJobSearch.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>{{id}}</h1>
+  </div>
+</template>
+
+<script>
+// import DisplayTable from "@/components/DisplayTable.vue";
+import { mapGetters } from "vuex";
+import sharedMethods from '../sharedMethods'
+export default {
+  name: "batchJobSearch",
+  components: {
+    // DisplayTable: DisplayTable,
+  },
+  data() {
+    return {
+      id: "",
+    };
+  },
+
+  computed: {
+    ...mapGetters({
+      token: "getToken",
+    }),
+  },
+  created() {
+    this.showNotification = sharedMethods.showNotification;
+    const batchJobId = this.$route.params.batchJobId;
+    this.loadBatchJob(batchJobId)
+  },
+  methods: {
+    loadBatchJob(JobId) {
+      this.id = JobId;      
+    } 
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
