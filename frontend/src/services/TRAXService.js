986d558cad01764cd05a09c4c08f5b1d3c8fd0bf
Shaun <shaun.lum@gov.bc.ca>
2022-03-08 09:27:44 -0800

    added traxservice

---
+++ frontend/src/services/TRAXService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: process.env.VUE_APP_BASE_URL,
+});
+
+export default {
+    getPSI(token) {
+        const headers = { Accept: 'application/json','Content-Type': 'application/json',"Access-Control-Allow-Origin": "*",'Authorization': 'Bearer '+ token }
+        return apiClient.get('/api/v1/psi',{ headers });
+    },
+    getPSIByAdvanceSearch(params, token) {
+        const headers = { Accept: 'application/json','Content-Type': 'application/json',"Access-Control-Allow-Origin": "*",'Authorization': 'Bearer '+ token }
+        return apiClient.get('/api/v1/psi/search/?'  + params,{ headers });
+    },
+    getDistrict(district, token){
+        const headers = { Accept: 'application/json','Content-Type': 'application/json',"Access-Control-Allow-Origin": "*",'Authorization': 'Bearer '+ token }
+        return apiClient.get('/api/v1/district/'  + district ,{ headers });
+    }
+};
+
