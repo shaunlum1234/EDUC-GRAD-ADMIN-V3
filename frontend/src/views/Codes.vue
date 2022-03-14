5bb16a0410ae4d8a3ad6887755d0b79c16a00f44
Michael Tang <michael.tang@gov.bc.ca>
2022-03-14 13:47:56 -0700

    GRAD2-1041 fixes

--- frontend/src/views/Codes.vue
+++ frontend/src/views/Codes.vue
@@ -5,7 +5,7 @@
 <b-card title="Program Management" no-body>
     <b-card-header header-tag="nav" class="px-3">
       <b-nav card-header tabs >
-        <b-nav-item to="/codes/career-programs" :active="tab === 1" @click="tab = 1">Career Programs</b-nav-item>
+        <b-nav-item to="/codes/career-programs" :active="tab === 1" @click="tab = 1">Career Program Codes</b-nav-item>
          <b-nav-item-dropdown
             id="credentials-dropdown"
             text="Credentials"
@@ -20,10 +20,10 @@
             <b-nav-item to="/codes/signature-blocks" :active="tab === 2" @click="tab = 2; closeDropdown('credentials-dropdown')">Signature Block</b-nav-item>
             <b-nav-item to="/codes/document-status-codes" :active="tab === 2" @click="tab = 2; closeDropdown('credentials-dropdown')">Document Status Codes</b-nav-item>
         </b-nav-item-dropdown>
-        <b-nav-item to="/codes/report-types" :active="tab === 3" @click="tab = 3">Reports</b-nav-item>
+        <b-nav-item to="/codes/report-types" :active="tab === 3" @click="tab = 3">Student Level Report Types</b-nav-item>
         <b-nav-item to="/codes/student-status-codes" :active="tab === 4" @click="tab = 4">Student Status Codes</b-nav-item>
-        <b-nav-item to="/codes/ungrad-reasons" :active="tab === 5" @click="tab = 5">Ungrad Reasons</b-nav-item>
-        <b-nav-item to="/codes/history-activity" :active="tab === 6" @click="tab = 6">History Activity</b-nav-item>
+        <b-nav-item to="/codes/ungrad-reasons" :active="tab === 5" @click="tab = 5">Ungrad Reason Codes</b-nav-item>
+        <b-nav-item to="/codes/history-activity" :active="tab === 6" @click="tab = 6">History Activity Codes</b-nav-item>
         
       </b-nav>
     </b-card-header>
