42034677723fa9b5477e77af5ac2ba850e22086e
suzalflueck <samflueck95@gmail.com>
2022-04-05 10:48:08 -0700

    Added custom icons so student info arrow toggles direction when accordian expands

--- frontend/src/views/StudentProfile.vue
+++ frontend/src/views/StudentProfile.vue
@@ -15,8 +15,11 @@
             <tr>
               <td>
                 <b-button  v-b-toggle.student-accordion variant="link" v-on:click="moreStudentInfo = !moreStudentInfo">
-                  <img v-show="!moreStudentInfo" src="../assets/images/icon-right.svg" width="14px" aria-hidden="true" alt=""/>
-                  <img v-show="moreStudentInfo" src="../assets/images/icon-down.svg" height="8px" aria-hidden="true" alt=""/>
+                  <!-- <i class='fas fa-lg fa-caret-down' aria-hidden="true"></i> -->
+                  <!-- <object data="../assets/images/icon-down.svg" type="image/svg+xml">Why no work?</object> -->
+                  <!-- <object v-show="moreStudentInfo" data="../assets/images/icon-up.svg" type="image/svg+xml"></object> -->
+                  <img v-show="!moreStudentInfo" src="../assets/images/icon-down.svg" height="8px"/>
+                  <img v-show="moreStudentInfo" src="../assets/images/icon-up.svg" height="8px"/>
                 </b-button>
               </td>
               <td class="align-top profile-name-data px-0" v-if="studentFullName.pen"><strong><p class="profile-info">{{ studentFullName.pen }}</p></strong></td>
