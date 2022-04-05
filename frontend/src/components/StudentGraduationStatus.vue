44ee497a8e0711225214b68a20530b2c0c9f2eb3
suzalflueck <samflueck95@gmail.com>
2022-04-05 11:06:53 -0700

    Student Grad Status component now uses 3 cols

--- frontend/src/components/StudentGraduationStatus.vue
+++ frontend/src/components/StudentGraduationStatus.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="row">
       <!-- Left col -->  
-      <div class="col-12 pl-2 pr-0 col-md-7 ">
+      <div class="col-12 pl-2 pr-0 col-md-5 ">
           <div class="graduation-status">
           <b-card
             no-body
@@ -278,7 +278,7 @@
             </b-card>       
           </div> 
           <!-- GRADUATION REPORTS -->
-          <div class="graduation-reports">
+          <!-- <div class="graduation-reports">
       
             <b-card
               header="Student Transcript Reports"
@@ -305,10 +305,10 @@
                 </b-card-text>
             </b-card> 
             
-          </div>
+          </div> -->
 
           <!-- CERTIFICATION DOGWOODS -->           
-          <div class="certification-dogwoods">
+          <!-- <div class="certification-dogwoods">
             <b-card
               header="Student Certificates/Dogwoods"
               no-body
@@ -327,10 +327,10 @@
                 </div>
               </b-card-text>
             </b-card> 
-          </div>           
+          </div>            -->
       </div>
       <!-- Right Column -->
-      <div class="col-12 px-2 col-md-5"> 
+      <div class="col-12 px-2 col-md-3"> 
 
         <div class="requirements-met-and-not-met">
           <div class="requirements-not-met">
@@ -375,6 +375,60 @@
             </b-card>
           </div>
         </div>
+      </div>
+      <!-- Mid Column -->
+      <div class="col-12 px-2 col-md-4">
+        <!-- GRADUATION REPORTS -->
+          <div class="graduation-reports">
+      
+            <b-card
+              header="Student Transcript Reports"
+              no-body
+            >
+      
+                <b-card-text class="py-4">
+                    <div v-for="report in reports" :key="report.gradReportTypeCode+report.updatedTimestamp" class="px-3 w-100 float-left">
+                      <a  @click="downloadPDF(report.report,'application/pdf')" href="#"  class="pdf-link float-left mt-2">{{report.gradReportTypeLabel}} (PDF)</a> 
+                      <div class="float-left col-12 pr-4 ml-1">
+                          <strong>Status:</strong> {{report.documentStatusLabel}} 
+                          <strong>Last Updated:</strong> {{report.updatedTimestamp}} 
+                          <strong>Distributed:</strong> {{report.distributionDate}}
+                      </div>
+                    </div>
+                    <div v-for="transcript in transcripts" :key="transcript.id" class="px-3 w-100 float-left mt-2">
+                      <a  @click="downloadPDF(transcript.transcript,'application/pdf')" href="#"  class="pdf-link float-left ">{{transcript.transcriptTypeLabel}} (PDF)</a> 
+                      <div class="float-left col-12 pr-4 ml-1">
+                          <strong>Status:</strong> {{transcript.documentStatusLabel}} 
+                          <strong>Last Updated:</strong> {{transcript.updatedTimestamp}} 
+                          <strong>Distributed:</strong> {{transcript.distributionDate}}
+                      </div>
+                    </div>                    
+                </b-card-text>
+            </b-card> 
+            
+          </div>
+          
+          <!-- CERTIFICATION DOGWOODS -->           
+          <div class="certification-dogwoods">
+            <b-card
+              header="Student Certificates/Dogwoods"
+              no-body
+             
+            >
+              <b-card-text class="py-4">
+                <div v-for="certificate in certificates" :key="certificate.gradCertificateTypeCode+certificate.createdTimestamp" class="px-3 w-100 float-left">
+                  
+                  <a @click="downloadPDF(certificate.certificate,'application/pdf')" href="#"  class="pdf-link float-left ">{{certificate.gradCertificateTypeLabel}} (PDF)</a> 
+                  <span class="float-right pr-3">
+                    <strong>Status:</strong> {{certificate.documentStatusLabel}} 
+                    <strong>Last Updated:</strong> {{certificate.createdTimestamp}}
+                    <strong>Distributed:</strong> {{certificate.distributionDate}}
+                  </span>
+                
+                </div>
+              </b-card-text>
+            </b-card> 
+          </div> 
       </div>
     </div>
 
