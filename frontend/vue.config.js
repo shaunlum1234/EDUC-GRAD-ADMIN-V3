e7204d77aa885db9efccee99f44c56cabf2a3a96
suzalflueck <samflueck95@gmail.com>
2022-05-17 10:35:30 -0700

    Updated api calls from ungrad to undo completion

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -68,7 +68,7 @@ module.exports = {
         target: process.env.VUE_APP_STUDENT_GRADUATION_API_HOST,
         changeOrigin: true,
       },
-      '/api/v1/ungrad/': {
+      '/api/v1/undocompletion/': {
         target: process.env.VUE_APP_STUDENT_GRADUATION_API_HOST,
         changeOrigin: true,
       },
