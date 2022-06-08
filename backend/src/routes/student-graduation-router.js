426d75d5a31a530f0502036b9001776815c6fda1
Shaun Lum <shaun.lum@gov.bc.ca>
2022-06-08 16:31:05 -0700

    added debug

--- backend/src/routes/student-graduation-router.js
+++ backend/src/routes/student-graduation-router.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 const config = require('../config/index');
 const auth = require('../components/auth');
-const { errorResponse, getBackendToken, getData, putData} = require('../components/utils');
+const { errorResponse, getBackendToken, getData, postData, putData} = require('../components/utils');
 
 //Program Routes
 router.get('*',passport.authenticate('jwt', {session: false}, undefined), getStudentGraduationAPI);
+router.post('*',passport.authenticate('jwt', {session: false}, undefined), postStudentGraduationAPI);
 
 async function getStudentGraduationAPI(req, res) {
   const token = getBackendToken(req);
@@ -14,6 +15,17 @@ async function getStudentGraduationAPI(req, res) {
   try {
     const url = `${config.get('server:studentGraduationAPIURL')}` + req.originalUrl;
     const data = await getData(token, url);
+    return res.status(200).json(data);
+  } catch (e) {
+      return errorResponse(res);
+  }
+}
+async function postStudentGraduationAPI(req, res) {
+  const token = getBackendToken(req);
+  console.log(req)
+  try {
+    const url = `${config.get('server:studentGraduationAPIURL')}` + req.originalUrl;
+    const data = await postData(token, url);
     return res.status(200).json(data);
   } catch (e) {
       return errorResponse(res);
