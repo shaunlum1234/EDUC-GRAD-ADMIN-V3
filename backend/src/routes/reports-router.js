68e42f2fea68da08d48e9d8c12ee333cde3d5d6f
Shaun Lum <shaun.lum@gov.bc.ca>
2022-06-14 09:19:17 -0700

    adde reports routed

---
+++ backend/src/routes/reports-router.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const express = require('express');
+const router = express.Router();
+const config = require('../config/index');
+const auth = require('../components/auth');
+const { errorResponse, getBackendToken, getData, postData, putData, deleteData} = require('../components/utils');
+
+//Program Routes
+router.get('*',passport.authenticate('jwt', {session: false}, undefined), getReportsAPI);
+
+async function getReportsAPI(req, res) {
+  const token = getBackendToken(req);
+  try {
+    const url = `${config.get('server:reportsAPIURL')}/reports` + req.url;
+    const data = await getData(token, url);
+    return res.status(200).json(data);
+  } catch (e) {
+      return errorResponse(res);
+  }
+}
+
+module.exports = router;
