ba7dd46ab1fec450980c275a9f50f197889ffadd
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-06-28 13:46:16 -0700

    Update roles.js
    Removed log data

--- test/src/config/roles.js
+++ test/src/config/roles.js
@@ -3,8 +3,6 @@ import { base_url, credentials } from './constants';
 import log from "npmlog";
 
 const adminUser = Role(base_url + '/?login=noidir', async t => {
-    log.info('Inputting credentials for: ');
-    log.info(credentials.adminCredentials.username);
     await t
         .typeText('#username', credentials.adminCredentials.username)
         .typeText('#password', credentials.adminCredentials.password)
