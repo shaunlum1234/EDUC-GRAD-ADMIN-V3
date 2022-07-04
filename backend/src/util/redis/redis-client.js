47dad3d325ea4487f727a5a0c40bbfe24c2a7db7
Kamal <kamal.mohammed@outlook.com>
2022-07-04 14:21:58 -0600

    updates

--- backend/src/util/redis/redis-client.js
+++ backend/src/util/redis/redis-client.js
@@ -14,7 +14,7 @@ const Redis = {
     if('local' === config.get('environment')){
       redisClient = new IOREDIS({
         host: config.get('redis:host'),
-        port: config.get('redis:port')
+        port: config.get('redis:port'),
         password: config.get('redis:password')
       });
     } else {
