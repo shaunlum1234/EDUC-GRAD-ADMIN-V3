e0c41452b89a0bd04d12a42c1de4204ac1ed11d9
Michael Tang <michael.tang@gov.bc.ca>
2022-07-08 15:28:32 -0700

    fixes for showing reports

--- frontend/src/components/GraduationStatus/StudentGraduationReports.vue
+++ frontend/src/components/GraduationStatus/StudentGraduationReports.vue
@@ -5,13 +5,16 @@
       no-body
     >   
       <b-card-text class="py-4">
-        <div v-if="!(studentGradStatus.studentGradData.school && studentGradStatus.studentGradData.school.transcriptEligibility === 'Y')">
-          <b-alert show variant="info" class="p-3 mb-1 mx-3">
-            <h4 class="alert-heading">Ineligible for Ministry transcripts</h4>
-            <p class="locked-message">
-              This student's school is ineligible for Ministry transcripts.
-            </p>
-        </b-alert>
+        <div v-if="studentGradStatus.studentGradData">
+          <div v-if="!(studentGradStatus.studentGradData.schoolOfRecord && studentGradStatus.studentGradData.transcriptEligibility === 'Y')">
+            <b-alert show variant="info" class="p-3 mb-1 mx-3">
+              <h4 class="alert-heading">Ineligible for Ministry transcripts</h4>
+              <p class="locked-message">
+                This student's school is ineligible for Ministry transcripts.
+              </p>
+            </b-alert>
+          </div>   
+        <div v-if="reports">
           <div v-for="(report, index) in reports" :key="index" class="px-3 w-100 float-left">
             <a  @click="downloadPDF(report.report,'application/pdf')" href="#" class="pdf-link float-left mt-2">{{report.gradReportTypeLabel}} (PDF)</a> 
             <div class="float-left col-12 pr-4 ml-1">
@@ -28,7 +31,7 @@
               </ul>
             </div>
           </div>
-        </div>
+          </div>        
         <div v-if="transcripts">
           <div  v-for="transcript in transcripts" :key="transcript.id" class="px-3 w-100 float-left mt-2">
             <a  @click="downloadPDF(transcript.transcript,'application/pdf')" href="#"  class="pdf-link float-left ">{{transcript.transcriptTypeLabel}} (PDF)</a> 
@@ -48,10 +51,11 @@
           </div>    
         </div>  
         <div>
-          <div v-if="xmlReports != 'not loaded'" class="px-3 w-100 float-left mt-2">
-              <b-button variant="link" :disabled="!(studentGradStatus.studentGradData.school && studentGradStatus.studentGradData.school.transcriptEligibility === 'Y')" @click="downloadPDF(xmlReports,'application/pdf')" href="#">View XML Preview</b-button>
+          <div class="px-3 w-100 float-left mt-2">
+              <b-button variant="link" :disabled="!(studentGradStatus.studentGradData.schoolOfRecord && studentGradStatus.studentGradData.transcriptEligibility === 'Y')" @click="downloadPDF(xmlReports,'application/pdf')" href="#">View XML Preview</b-button>
           </div>    
         </div>                          
+      </div> 
       </b-card-text>
     </b-card>       
   </div>
@@ -66,8 +70,8 @@ import sharedMethods from '../../sharedMethods';
     props: {},
     computed: {
       ...mapGetters({
-        reports: "getStudentReports",
         transcripts: "getStudentTranscripts",
+        reports: "getStudentReports",
         xmlReports: "getStudentXmlReports",
         studentGradStatus: "getStudentGradStatus"
       })
