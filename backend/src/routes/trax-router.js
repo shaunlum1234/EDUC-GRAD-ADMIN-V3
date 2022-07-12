a1976195e05d45a54fc5600743bf6aadf2e6b3d8
Jinil Sung <infstar@gmail.com>
2022-07-12 11:58:15 -0700

    Update trax-router.js
    added some logs to debug trax api request url and params to track on openshift side.

--- backend/src/routes/trax-router.js
+++ backend/src/routes/trax-router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const config = require('../config/index');
 const auth = require('../components/auth');
 const roles = require("../components/roles");
+const log = require('../components/logger');
 const { errorResponse, getBackendToken, getData, postData, putData, deleteData} = require('../components/utils');
 const isValidUiTokenWithStaffRoles = auth.isValidUiTokenWithRoles('GRAD_SYSTEM_COORDINATOR', [roles.Admin.StaffAdministration]);
 
@@ -14,6 +15,8 @@ async function getTRAXAPI(req, res) {
   const token = getBackendToken(req);
   try {
     const url = `${config.get('server:gradTraxAPIURL')}/trax` + req.url;
+    log.info('TRAX API - the request URL ==> ' + req.url);
+    log.info('TRAX API - the full URL ==> ' + url);
     const data = await getData(token, url);
     return res.status(200).json(data);
   } catch (e) {
