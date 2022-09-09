5d6e8da555b2620efb2027688a44a0cd383e4d36
Michael Tang <michael.tang@gov.bc.ca>
2022-09-09 11:12:40 -0700

    messaging changes to backend

--- backend/src/routes/graduation-reports-router.js
+++ backend/src/routes/graduation-reports-router.js
@@ -18,7 +18,11 @@ async function getStudentAPI(req, res) {
     const data = await getData(token, url);
     return res.status(200).json(data);
   } catch (e) {
+    if(e.data.message){
+      return errorResponse(res, e.data.message, e.status);
+    } else {
       return errorResponse(res);
+    }
   }
 }
 
