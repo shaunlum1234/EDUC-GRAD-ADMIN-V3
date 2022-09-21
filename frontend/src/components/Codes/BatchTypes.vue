10b8ab132c73a32d4054e7052d3a96f64c5a0c22
Shaun Lum <shaun.lum@gov.bc.ca>
2022-09-21 09:11:26 -0700

    added batch component

---
+++ frontend/src/components/Codes/BatchTypes.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <p>A list of Batch Runs used by the GRAD system.</p>
+  <DisplayTable
+    title="Batch Types"
+    v-bind:items="batchTypes"
+    v-bind:fields="batchTypesFields"
+    id=""
+    showFilter="true"
+  >
+  </DisplayTable>
+  </div>
+</template>
+
+<script>
+import DisplayTable from "@/components/DisplayTable";
+import BatchProcessingService from "@/services/BatchProcessingService.js";
+
+
+export default {
+  name: 'BatchTypes',
+  components: {
+    DisplayTable: DisplayTable,
+  },
+  created() {
+      BatchProcessingService.getBatchJobTypes()
+      .then((response) => {
+        this.batchTypes = response.data;
+      })
+      // eslint-disable-next-line
+      .catch((error) => {
+        this.$bvToast.toast("ERROR " + error.response.statusText, {
+          title: "ERROR" + error.response.status,
+          variant: "danger",
+          noAutoHide: true,
+        });
+      });
+  },
+  data: function() {
+    return {
+      batchTypes: [],
+      batchTypesFields: [
+        {
+          key: "code",
+          label: "Code",
+          sortable: true,
+          sortDirection: "desc",
+        },
+        {
+          key: "label",
+          label: "Label",
+          sortable: true,
+        },
+      ],
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+.table th, .table td{
+  border-top: none !important;
+
+}</style>
