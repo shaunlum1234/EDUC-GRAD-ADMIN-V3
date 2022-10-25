c9708c1684ceaf4c09eabd51cd0cdd038a57ff98
Michael Tang <michael.tang@gov.bc.ca>
2022-10-25 13:02:56 -0700

    GRAD2-1162

--- frontend/src/components/GraduationStatus/GRADStatus.vue
+++ frontend/src/components/GraduationStatus/GRADStatus.vue
@@ -22,6 +22,7 @@
         <div v-if="studentGradStatus">
         <div v-if="studentGradStatus.studentGradData">
         <div v-if="studentGradStatus.studentGradData.gradStatus"> 
+          <!-- If grad recalc flag = “Y” and tvr recalc flag = “Y” -->
           <div v-if="studentGradStatus.studentGradData.gradStatus.recalculateGradStatus == 'Y' && studentGradStatus.recalculateProjectedGrad == 'Y'">
             <b-alert show variant="info" class="p-3 mb-1">
               <h4 class="alert-heading">Gradution data has changed for this student</h4>
@@ -36,7 +37,38 @@
                 NOTE: this message will remain until the student gets run through the nightly batch processes to ensure the projected graduation report, the graduation report and the non-graduate report are updated for the School Secure WEB (TSW).
               </p>
             </b-alert>
-          </div>     
+          </div> 
+          <!-- If grad recalc flag = “Y” and tvr recalc flag = blank or “N” -->
+          <div v-if="studentGradStatus.studentGradData.gradStatus.recalculateGradStatus == 'Y' && studentGradStatus.recalculateProjectedGrad != 'Y'">
+            <b-alert show variant="info" class="p-3 mb-1">
+              <h4 class="alert-heading">Gradution data has changed for this student</h4>
+              <p class="locked-message">
+                The students' data will get updated tonight via the batch processes. However you can also immediately update the students' data as follows:
+              </p>
+              <ul class="locked-message">
+                <li>Run the Graduate Student to update the student graduation status and credentials or run Update Student Reports if the student has graduated</li>
+              </ul>
+              <p class="locked-message">
+                NOTE: this message will remain until the student gets run through the nightly batch processes to ensure the graduation report and the non-graduate report are updated for the School Secure WEB (TSW).
+              </p>
+            </b-alert>
+          </div> 
+          <!-- If grad recalc flag = blank or “N” and tvr recalc flag = “Y” -->
+          <div v-if="studentGradStatus.studentGradData.gradStatus.recalculateGradStatus != 'Y' && studentGradStatus.recalculateProjectedGrad == 'Y'">
+            <b-alert show variant="info" class="p-3 mb-1">
+              <h4 class="alert-heading">Graduation data has changed for this student and has been updated, however, the students' TVR is not up-to-date.</h4>
+              <p class="locked-message">
+                The students' TVR data will get updated tonight via the batch processes.  However you can also immediately update the students' data as follows:
+              </p>
+              <ul class="locked-message">
+                <li>Run the Projected final marks and registrations to ensure the students’ TVR report is up-to-date.</li>
+              </ul>
+              <p class="locked-message">
+                NOTE: this message will remain until the student gets run through the nightly batch processes to ensure the projected graduation report is updated for the School Secure WEB (TSW).
+              </p>
+            </b-alert>
+          </div> 
+        <!-- gradStatus checked -->
         </div>      
         </div>  
         </div>
